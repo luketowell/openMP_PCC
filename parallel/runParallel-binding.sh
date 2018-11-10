2d59d60c58dbb14b76454ef1ad76a93be075f404
Unknown <luke.towell@hotmail.com>
2018-11-10 19:07:25 +0000

    changed the runParallel script to bind to threads
    - changed the run parallel script to enable omp-proc-binding which binds the threads to sets of variables.

---
+++ parallel/runParallel-binding.sh
@@ -0,0 +1,28 @@
+usage(){
+        echo $0 ./parallelCode.exe numThreads numRepeats arrayLength 
+}
+
+if [[ $# -ne 3 ]]; then
+        usage
+        exit -1
+fi
+
+EXE=parallelCode.exe
+THREADS=$1
+REPEATS=$2
+ARRAY_LENGTH=$3
+FILE=parallel${ARRAY_LENGTH}on${THREADS}by${REPEATS}_binding.txt
+
+export OMP_PROC_BIND=TRUE
+export OMP_DYNAMIC=FALSE
+
+echo best of $REPEATS for executable $EXE
+
+        export OMP_NUM_THREADS=$THREADS
+        echo running ${EXE} with an array length of ${ARRAY_LENGTH}
+        echo running on $OMP_NUM_THREADS threads
+        for k in `seq 1 $REPEATS`; do
+                ./${EXE} ${ARRAY_LENGTH}
+        done  > $FILE
+
+echo Finished

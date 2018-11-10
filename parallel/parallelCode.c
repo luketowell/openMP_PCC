40cb4371212774c64224a3b46e5ed4c8b371170c
Unknown <luke.towell@hotmail.com>
2018-11-10 13:52:55 +0000

    Added in the Parallel code
    - Added in the parallel code which is based on the Serial Code.
    - The code contains a for loop for the initialisation of the arrays.
    
    - I have also swapped out the timing function to use omp-get-wtime();

---
+++ parallel/parallelCode.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+        //declare all int values
+        int length_of_array = strtol(argv[1], NULL, 10);
+        int i;
+
+        //declaration of clocks
+        double start, end;
+
+        //declare all doubles
+        double *a, *b;
+        double sum_of_a, sum_of_b, mean_of_a, mean_of_b, sum_of_sqrd_a_a_bar, sum_of_sqrd_b_b_bar , sum_of_a_times_b, correlationCoefficient, time_to_run;
+
+        //allocate memory to the arrays
+        a = (double *) malloc(length_of_array * sizeof(a));
+        b = (double *) malloc(length_of_array * sizeof(b));
+
+        start= omp_get_wtime();
+        #pragma omp parallel for default(none) private(i) shared(length_of_array, a, b)
+        //initialisation of the value arrays
+        for (i = 0; i < length_of_array; i++){
+                a[i] = sin(i);
+                b[i] = sin(i+5);
+        }
+
+
+        for (i = 0; i < length_of_array; i++){
+                sum_of_a += a[i];
+                sum_of_b += b[i];
+        }
+
+        //work out the mean
+        mean_of_a = sum_of_a / length_of_array;
+        mean_of_b = sum_of_b / length_of_array;
+
+        printf("print the mean of a = %.10f \n",mean_of_a);
+        printf("print the mean of b = %.10f \n",mean_of_b);
+
+        //perform work out coefficients
+
+        for (i = 0; i < length_of_array; i++){
+                sum_of_sqrd_a_a_bar += (a[i] - mean_of_a) * (a[i] - mean_of_a); //Why square them myself instead of using a function like pow
+                sum_of_sqrd_b_b_bar += (b[i] - mean_of_b) * (b[i] - mean_of_b);
+                sum_of_a_times_b += (a[i] - mean_of_a) * (b[i] - mean_of_b);
+        }
+        correlationCoefficient = sum_of_a_times_b / sqrt((sum_of_sqrd_a_a_bar * sum_of_sqrd_b_b_bar));
+
+        end = omp_get_wtime();
+
+        time_to_run = (end - start);
+        printf("The time taken to execute the whole program = %.5f ms \n", (time_to_run*1000));
+        printf("The Pearson correlation coefficient is: %.4f \n", correlationCoefficient);
+
+        return 0;
+}

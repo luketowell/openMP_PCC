71cc0111683ca33f39e8b46087c57caafba181f1
Unknown <luke.towell@hotmail.com>
2018-11-10 19:10:07 +0000

    Serial Timing changes
    Removed the timing function that was initially being used for timings and then added in more timings to replicate timings that are currently being used in the parallel code with omp_get_wtime().

--- serial/serialCode.c
+++ serial/serialCode.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
         int i;
 
         //declaration of clocks
-        double start, end;
+        double init_start, init_end, sum_start, sum_end, coef_start, coef_end, start, end;
 
         //declare all doubles
         double *a, *b;
@@ -24,18 +24,22 @@ int main(int argc, char **argv)
         a = (double *) malloc(length_of_array * sizeof(a));
         b = (double *) malloc(length_of_array * sizeof(b));
 
-        start = get_wall_time();
+        start = omp_get_wtime();
+        init_start= omp_get_wtime();
         //initialisation of the value arrays
         for (i = 0; i < length_of_array; i++){
                 a[i] = sin(i);
                 b[i] = sin(i+5);
         }
+        init_end = omp_get_wtime();
 
+        sum_start = omp_get_wtime();
         //sum up arrays
         for (i = 0; i < length_of_array; i++){
                 sum_of_a += a[i];
                 sum_of_b += b[i];
         }
+        sum_end = omp_get_wtime();
 
         //work out the mean
         mean_of_a = sum_of_a / length_of_array;
@@ -45,27 +49,23 @@ int main(int argc, char **argv)
         printf("print the mean of b = %.10f \n",mean_of_b);
 
         //perform work out coefficients
-
+        coef_start = omp_get_wtime();
         for (i = 0; i < length_of_array; i++){
                 sum_of_sqrd_a_a_bar += (a[i] - mean_of_a) * (a[i] - mean_of_a); //Why square them myself instead of using a function like pow
                 sum_of_sqrd_b_b_bar += (b[i] - mean_of_b) * (b[i] - mean_of_b);
                 sum_of_a_times_b += (a[i] - mean_of_a) * (b[i] - mean_of_b);
         }
+        coef_end= omp_get_wtime();
         correlationCoefficient = sum_of_a_times_b / sqrt((sum_of_sqrd_a_a_bar * sum_of_sqrd_b_b_bar));
 
-        end = get_wall_time();
+        end = omp_get_wtime();
 
         time_to_run = (end - start);
+        printf("The time to initialise arrays was %.5f ms \n", ((init_end-init_start) *1000));
+        printf("The time to work out the sum is: %.5f ms \n", ((sum_end-sum_start) *1000));
+        printf("The time to work out coefficient was %.5f ms \n", ((coef_end-coef_start)*1000));
         printf("The time taken to execute the whole program = %.5f ms \n", (time_to_run*1000));
         printf("The Pearson correlation coefficient is: %.4f \n", correlationCoefficient);
 
         return 0;
-}
-
-double get_wall_time(){
-        struct timeval time;
-        if(gettimeofday(&time, NULL)){
-                return 0;
-        }
-        return (double)time.tv_sec + (double)time.tv_usec * .000001;
 }

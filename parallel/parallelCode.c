fc9f60f0e6f588bd86f09b29a5df29542aa89106
Unknown <luke.towell@hotmail.com>
2018-11-10 18:11:15 +0000

    Parallelised the coefficient
    Parallelised the coefficient function so that it now runs calculates the coefficient in parallel.

--- parallel/parallelCode.c
+++ parallel/parallelCode.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
         int i;
 
         //declaration of clocks
-        double init_start, init_end, start, end;
+        double init_start, init_end, coef_start, coef_end, start, end;
 
         //declare all doubles
         double *a, *b;
@@ -29,7 +29,6 @@ int main(int argc, char **argv)
         }
         init_end = omp_get_wtime();
 
-
         for (i = 0; i < length_of_array; i++){
                 sum_of_a += a[i];
                 sum_of_b += b[i];
@@ -43,18 +42,22 @@ int main(int argc, char **argv)
         printf("print the mean of b = %.10f \n",mean_of_b);
 
         //perform work out coefficients
-
+        coef_start = omp_get_wtime();
+        #pragma omp parallel for default(none) shared(length_of_array, a, mean_of_a, b, mean_of_b) private(i) reduction(+:sum_of_sqrd_a_a_bar, sum_of_sqrd_b_b_bar, sum_of_a_times_b)
         for (i = 0; i < length_of_array; i++){
                 sum_of_sqrd_a_a_bar += (a[i] - mean_of_a) * (a[i] - mean_of_a); //Why square them myself instead of using a function like pow
                 sum_of_sqrd_b_b_bar += (b[i] - mean_of_b) * (b[i] - mean_of_b);
                 sum_of_a_times_b += (a[i] - mean_of_a) * (b[i] - mean_of_b);
         }
+        coef_end = omp_get_wtime();
+
         correlationCoefficient = sum_of_a_times_b / sqrt((sum_of_sqrd_a_a_bar * sum_of_sqrd_b_b_bar));
 
         end = omp_get_wtime();
 
         time_to_run = (end - start);
         printf("The time to initialise arrays was %.5f ms \n", ((init_end-init_start) *1000));
+        printf("The time to work out coefficient was %.5f ms \n", ((coef_end-coef_start)*1000));
         printf("The time taken to execute the whole program = %.5f ms \n", (time_to_run*1000));
         printf("The Pearson correlation coefficient is: %.4f \n", correlationCoefficient);
 
